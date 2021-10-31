182b208073e7dc60f7aa41f4b61cd40581b93470
YUVARAJ <72031721+yuvaraj-saran@users.noreply.github.com>
2021-10-31 17:55:23 +0530

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+const http=require('http');
+http.createServer(function(req,res) 
+{
+res.writeHead(200,{'Content-Type':'text/html'});
+res.write('<h1>Hello World</h1>');
+res.write('<h2>Hello World</h2>');
+res.end();
+}).listen(8080);	
